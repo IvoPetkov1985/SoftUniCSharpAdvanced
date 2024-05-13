1fa9fb77e2a30402dbd2c64dbe610b0993dd4202
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-05-13 15:01:28 +0300

    Task 7 from Stacks and Queues

---
+++ 01. Stacks and Queues/7. Hot Potato 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            string[] tokens = inputLine
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> kids = new Queue<string>(tokens);
+
+            int tosses = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < tosses - 1; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {kids.Peek()}");
+        }
+    }
+}
---
+++ 01. Stacks and Queues/7. Hot Potato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> kids = new Queue<string>(names);
+
+            int tosses = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 1; i < tosses; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+
+                string kidToDequeue = kids.Dequeue();
+                Console.WriteLine($"Removed {kidToDequeue}");
+            }
+
+            string lastKid = kids.Peek();
+            Console.WriteLine($"Last is {lastKid}");
+        }
+    }
+}
