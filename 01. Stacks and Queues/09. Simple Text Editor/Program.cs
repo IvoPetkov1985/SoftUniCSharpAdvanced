b4bedbd15cc4d18c0826b98111ca29075bc4a96e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-05-13 15:04:45 +0300

    Tasks 9 and 10 from Stacks and Queues

---
+++ 01. Stacks and Queues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Empty;
+
+            Stack<string> textModifications = new Stack<string>();
+
+            int commandsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string commandLine = Console.ReadLine();
+                string[] tokens = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int command = int.Parse(tokens[0]);
+
+                if (command == 1)
+                {
+                    textModifications.Push(text);
+                    text += tokens[1];
+                }
+                else if (command == 2)
+                {
+                    textModifications.Push(text);
+                    int countToErase = int.Parse(tokens[1]);
+                    text = text.Remove(text.Length - countToErase);
+                }
+                else if (command == 3)
+                {
+                    int index = int.Parse(tokens[1]) - 1;
+                    Console.WriteLine(text[index]);
+                }
+                else if (command == 4)
+                {
+                    text = textModifications.Pop();
+                }
+            }
+        }
+    }
+}
---
+++ 01. Stacks and Queues/10. Crossroads/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10._Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightDuration = int.Parse(Console.ReadLine());
+            int freeWindowDuration = int.Parse(Console.ReadLine());
+
+            Queue<string> carsQueue = new Queue<string>();
+            int counter = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command != "green")
+                {
+                    string car = command;
+                    carsQueue.Enqueue(car);
+                }
+                else
+                {
+                    int seconds = greenLightDuration;
+                    int addedSeconds = freeWindowDuration;
+
+                    while (carsQueue.Any() && seconds > 0)
+                    {
+                        string passingCar = carsQueue.Dequeue();
+
+                        if (seconds - passingCar.Length >= 0)
+                        {
+                            seconds -= passingCar.Length;
+                            counter++;
+                            continue;
+                        }
+
+                        if (seconds + addedSeconds - passingCar.Length >= 0)
+                        {
+                            counter++;
+                            continue;
+                        }
+
+                        int hitIndex = seconds + addedSeconds;
+
+                        Console.WriteLine("A crash happened!");
+                        Console.WriteLine($"{passingCar} was hit at {passingCar[hitIndex]}.");
+                        return;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{counter} total cars passed the crossroads.");
+        }
+    }
+}
