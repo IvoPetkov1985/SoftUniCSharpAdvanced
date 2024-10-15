741cec3aac532ab036ffc2398e47549a00b48693
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-15 15:49:10 +0300

    Tasks 7 and 8 uploaded.

---
+++ 03. Sets and Dictionaries Advanced/7. Parking Lot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _7._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            HashSet<string> allCars = new HashSet<string>();
+
+            while (inputLine != "END")
+            {
+                string[] tokens = inputLine
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = tokens[0];
+                string carNumber = tokens[1];
+
+                switch (direction)
+                {
+                    case "IN":
+                        allCars.Add(carNumber);
+                        break;
+
+                    case "OUT":
+                        allCars.Remove(carNumber);
+                        break;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            if (!allCars.Any())
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (string car in allCars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
---
+++ 03. Sets and Dictionaries Advanced/8. SoftUni Party/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _8._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            while (input != "END")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Remove(input);
+                }
+                else
+                {
+                    regularGuests.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int totalCount = vipGuests.Count + regularGuests.Count;
+            Console.WriteLine(totalCount);
+
+            foreach (string guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (string guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
