3f5d8ab90ad0566734d8b46fd1b0340a7eac24da
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-15 15:49:57 +0300

    Last two tasks uploaded.

---
+++ 03. Sets and Dictionaries Advanced/09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _09._SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            Dictionary<string, int> usersPoints = new Dictionary<string, int>();
+            Dictionary<string, int> languageSubmissions = new Dictionary<string, int>();
+
+            while (inputLine != "exam finished")
+            {
+                string[] tokens = inputLine.Split("-", StringSplitOptions.RemoveEmptyEntries);
+                string username = tokens[0];
+
+                if (tokens[1] == "banned")
+                {
+                    if (usersPoints.ContainsKey(username))
+                    {
+                        usersPoints.Remove(username);
+                        inputLine = Console.ReadLine();
+                        continue;
+                    }
+                }
+
+                string language = tokens[1];
+                int points = int.Parse(tokens[2]);
+
+                if (!usersPoints.ContainsKey(username))
+                {
+                    usersPoints.Add(username, 0);
+                }
+
+                if (usersPoints[username] < points)
+                {
+                    usersPoints[username] = points;
+                }
+
+                if (!languageSubmissions.ContainsKey(language))
+                {
+                    languageSubmissions.Add(language, 0);
+                }
+
+                languageSubmissions[language]++;
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine("Results:");
+
+            usersPoints = usersPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (KeyValuePair<string, int> kvp in usersPoints)
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value}");
+            }
+
+            Console.WriteLine("Submissions:");
+
+            languageSubmissions = languageSubmissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (KeyValuePair<string, int> kvp in languageSubmissions)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ 03. Sets and Dictionaries Advanced/10. ForceBook/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10._ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            Dictionary<string, SortedSet<string>> book = new Dictionary<string, SortedSet<string>>();
+
+            while (inputLine != "Lumpawaroo")
+            {
+                if (inputLine.Contains("|"))
+                {
+                    string[] tokens = inputLine.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceSide = tokens[0];
+                    string user = tokens[1];
+
+                    if (!book.Values.Any(x => x.Contains(user)))
+                    {
+                        if (!book.ContainsKey(forceSide))
+                        {
+                            book.Add(forceSide, new SortedSet<string>());
+                        }
+
+                        book[forceSide].Add(user);
+                    }
+                }
+                else
+                {
+                    string[] tokens = inputLine.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string user = tokens[0];
+                    string forceSide = tokens[1];
+
+                    foreach (KeyValuePair<string, SortedSet<string>> kvp in book)
+                    {
+                        if (kvp.Value.Contains(user))
+                        {
+                            kvp.Value.Remove(user);
+                            break;
+                        }
+                    }
+
+                    if (!book.ContainsKey(forceSide))
+                    {
+                        book.Add(forceSide, new SortedSet<string>());
+                    }
+
+                    book[forceSide].Add(user);
+
+                    Console.WriteLine($"{user} joins the {forceSide} side!");
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            book = book.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (KeyValuePair<string, SortedSet<string>> kvp in book)
+            {
+                if (kvp.Value.Any())
+                {
+                    Console.WriteLine($"Side: {kvp.Key}, Members: {kvp.Value.Count}");
+
+                    foreach (string user in kvp.Value)
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+    }
+}
