85cf39e9f93d6d1f6f2c849c50c52483b7599497
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-16 17:44:30 +0300

    Fourth task uploaded.

---
+++ 06. Defining Classes/04. Car Engine And Tires/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Tire[] tyres = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.5),
+                new Tire(1, 2.7),
+                new Tire(1, 2.7)
+            };
+
+            Engine engine = new Engine(90, 4200);
+
+            Car car = new Car("VW", "Golf", 2005, 80, 4, engine, tyres);
+        }
+    }
+}
