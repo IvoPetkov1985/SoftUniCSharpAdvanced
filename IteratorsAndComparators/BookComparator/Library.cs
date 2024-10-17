f6c065f06264d7bdcaa8d0ad03cdc091d0213b58
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-17 19:11:25 +0300

    First two tasks uploaded.

---
+++ IteratorsAndComparators/BookComparator/Library.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace BookComparator
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryEnumerator(books.ToArray());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
