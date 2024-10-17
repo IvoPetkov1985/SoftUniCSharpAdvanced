b24c73984185b1f0e38bbde5571de5fa58c5087e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-17 19:13:47 +0300

    ComparableBook uploaded.

---
+++ IteratorsAndComparators/ComparableBook/Library.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace ComparableBook
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryEnumerator(this.books.ToArray());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators/ComparableBook/LibraryEnumerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace ComparableBook
+{
+    public class LibraryEnumerator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+
+        private int currentIndex = -1;
+
+        public LibraryEnumerator(params Book[] books)
+        {
+            this.books = books.ToList();
+            this.Reset();
+        }
+
+        public Book Current => this.books[currentIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            return currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
