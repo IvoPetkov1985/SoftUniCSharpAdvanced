77712de3e8cc2713f444438915c1d3bc064ca008
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-19 14:21:13 +0300

    Another version of Implamenting LinkedList<int>

---
+++ DoublyLinkedList/DoublyLinkedList/SoftUniLinkedList.cs
@@ -0,0 +1,132 @@
+﻿namespace DoublyLinkedList
+{
+    internal class SoftUniLinkedList
+    {
+        private class ListNode
+        {
+            public ListNode(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; set; }
+
+            public ListNode Next { get; set; }
+
+            public ListNode Previous { get; set; }
+        }
+
+        private ListNode head = null!;
+
+        private ListNode tail = null!;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(element);
+                newHead.Next = head;
+                head.Previous = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                newTail.Previous = tail;
+                tail.Next = newTail;
+                tail = newTail;
+            }
+
+            Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int firstElement = head.Value;
+            head = head.Next;
+
+            if (head != null)
+            {
+                head.Previous = null;
+            }
+            else
+            {
+                tail = null;
+            }
+
+            Count--;
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int lastElement = tail.Value;
+            tail = tail.Previous;
+
+            if (tail != null)
+            {
+                tail.Next = null;
+            }
+            else
+            {
+                head = null;
+            }
+
+            Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] newArray = new int[Count];
+            int counter = 0;
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                newArray[counter] = currentNode.Value;
+                counter++;
+                currentNode = currentNode.Next;
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ DoublyLinkedList/DoublyLinkedList/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace DoublyLinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniLinkedList list = new SoftUniLinkedList();
+
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+            list.AddLast(40);
+
+            Console.WriteLine(list.RemoveLast());
+
+            list.AddFirst(15);
+
+            list.ForEach(x => Console.WriteLine(x));
+
+            int[] listAsArray = list.ToArray();
+
+            Console.WriteLine(string.Join(", ", listAsArray));
+        }
+    }
+}
