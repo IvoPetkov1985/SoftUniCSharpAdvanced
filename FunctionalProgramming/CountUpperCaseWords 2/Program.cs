085ac890d9ce2aa9d321a88254b51e17b9144e68
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-22 14:03:51 +0300

    Two solutions of the problem uploaded.

---
+++ FunctionalProgramming/CountUpperCaseWords 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUpperCaseWords_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<char, bool> isUpperCase = c => Char.IsUpper(c);
+
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => isUpperCase(w[0]))
+                .ToArray();
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUpperCaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isStartingWithUpper = str => str[0] == str.ToUpper()[0];
+
+            string[] allWords = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => isStartingWithUpper(w))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, allWords));
+        }
+    }
+}
