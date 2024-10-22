f8a2a6340dfd2460edb9b13ecaa0273c9425b8aa
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-22 14:02:17 +0300

    FunctionalProgramming tasks uploaded.

---
+++ FunctionalProgramming/AddVat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddVat
+{
+    internal class Program
+    {
+        delegate double VatAdder(double num);
+
+        static void Main(string[] args)
+        {
+            Func<string, double> doubleParser = x => double.Parse(x);
+
+            VatAdder addingVat = x => x * 1.2;
+
+            List<double> doubles = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(doubleParser)
+                .Select(x => addingVat(x))
+                .ToList();
+
+            doubles.ForEach(x => Console.WriteLine($"{x:F2}"));
+        }
+    }
+}
