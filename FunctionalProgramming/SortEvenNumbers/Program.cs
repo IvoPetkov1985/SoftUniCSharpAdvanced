57b3a093b6d59bfed2755d952a34caef39b05cd7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-22 14:08:43 +0300

    SortEvenNumbers added.

---
+++ FunctionalProgramming/SortEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = x => int.Parse(x);
+            Predicate<int> isEven = x => x % 2 == 0;
+
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .Where(x => isEven(x))
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
