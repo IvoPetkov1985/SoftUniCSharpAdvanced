39b6f8426ddb3847208f7f8010da62c4b760a9ac
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-23 18:12:19 +0300

    A problem about Higher order function

---
+++ FunctionalProgramming/HigherOrderFunction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HigherOrderFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long Aggregate(int start, int end, Func<long, long, long> oper)
+            {
+                long result = start;
+
+                for (long i = start + 1; i <= end; i++)
+                {
+                    result = oper(result, i);
+                }
+
+                return result;
+            }
+
+            Console.WriteLine(Aggregate(1, 10, (a, b) => a + b));
+            Console.WriteLine(Aggregate(1, 10, (a, b) => a * b));
+            Console.WriteLine(Aggregate(1, 20, (a, b) => a * b));
+            Console.WriteLine(Aggregate(1, 10, (a, b) => long.Parse("" + a + b)));
+
+            Random random = new Random();
+            Console.WriteLine(AggrFunc(1, 10, (x, y) => x + y + random.Next(1, 100)));
+
+            int AggrFunc(int start, int end, Func<int, int, int> func)
+            {
+                int result = start;
+
+                for (int i = start + 1; i <= end; i++)
+                {
+                    result = func(result, i);
+                }
+
+                return result;
+            }
+        }
+    }
+}
