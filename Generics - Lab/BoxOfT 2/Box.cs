eeb597254969e3c686a37f97a584bee73c590e1c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-24 17:38:17 +0300

    Lab Generics tasks uploaded.

---
+++ Generics - Lab/BoxOfT 2/Box.cs
@@ -0,0 +1,30 @@
+﻿namespace BoxOfT_2
+{
+    public class Box<T>
+    {
+        private Stack<T> data;
+
+        public Box()
+        {
+            this.data = new Stack<T>();
+        }
+
+        public void Add(T item)
+        {
+            data.Push(item);
+        }
+
+        public T Remove()
+        {
+            return data.Pop();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+    }
+}
---
+++ Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            return element;
+        }
+
+        public int Count { get { return data.Count; } }
+    }
+}
