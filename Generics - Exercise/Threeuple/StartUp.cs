f0db14dfd7456c85808b4c34aff23fc35403bd91
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-25 22:55:20 +0300

    Tuple and Threeuple tasks uploaded.

---
+++ Generics - Exercise/Threeuple/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAddressInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstName = nameAddressInput[0];
+            string lastName = nameAddressInput[1];
+            string fullName = $"{firstName} {lastName}";
+            string neighbourhood = nameAddressInput[2];
+            string city = string.Join(" ", nameAddressInput.Skip(3));
+
+            ThreeupleBox<string, string, string> addressThreeuple = new ThreeupleBox<string, string, string>(fullName, neighbourhood, city);
+
+            string[] drunkenInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string drunkardName = drunkenInput[0];
+            int liters = int.Parse(drunkenInput[1]);
+            bool isDrunk = drunkenInput[2] == "drunk";
+
+            ThreeupleBox<string, int, bool> drunkThreeuple = new ThreeupleBox<string, int, bool>(drunkardName, liters, isDrunk);
+
+            string[] bankInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string clientName = bankInput[0];
+            double balance = double.Parse(bankInput[1]);
+            string bankName = bankInput[2];
+
+            ThreeupleBox<string, double, string> bankThreeuple = new ThreeupleBox<string, double, string>(clientName, balance, bankName);
+
+            Console.WriteLine(addressThreeuple.ToString());
+            Console.WriteLine(drunkThreeuple.ToString());
+            Console.WriteLine(bankThreeuple.ToString());
+        }
+    }
+}
---
+++ Generics - Exercise/Tuple/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] addressInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] beerInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] numberInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstNameAddress = addressInput[0];
+            string lastNameAddress = addressInput[1];
+            string cityNameAddress = string.Join(" ", addressInput.Skip(2));
+            string fullNameAddress = $"{firstNameAddress} {lastNameAddress}";
+
+            TupleBox<string, string> address = new TupleBox<string, string>(fullNameAddress, cityNameAddress);
+
+            string consumerName = beerInput[0];
+            int liters = int.Parse(beerInput[1]);
+
+            TupleBox<string, int> beerTuple = new TupleBox<string, int>(consumerName, liters);
+
+            int firstNum = int.Parse(numberInput[0]);
+            double secondNum = double.Parse(numberInput[1]);
+
+            TupleBox<int, double> numberTuple = new TupleBox<int, double>(firstNum, secondNum);
+
+            Console.WriteLine(address.ToString());
+            Console.WriteLine(beerTuple.ToString());
+            Console.WriteLine(numberTuple.ToString());
+        }
+    }
+}
