2fa5f815bc542b2de7b35404be1e484c305dfb60
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-25 22:51:38 +0300

    GenericBoxOfInteger task uploaded.

---
+++ Generics - Exercise/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private List<T> collection;
+
+        public Box()
+        {
+            collection = new List<T>();
+        }
+
+        public void AddElement(T element)
+        {
+            collection.Add(element);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (T element in collection)
+            {
+                result.AppendLine($"{typeof(T)}: {element}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Generics - Exercise/GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Box<int> boxCollection = new Box<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                boxCollection.AddElement(currentNum);
+            }
+
+            Console.WriteLine(boxCollection.ToString());
+        }
+    }
+}
