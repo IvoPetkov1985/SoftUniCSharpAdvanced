f9381440c873367648a2e30d610bfb866565b2e0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-10-25 22:52:31 +0300

    GenericBoxOfString task uploaded.

---
+++ Generics - Exercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (T item in items)
+            {
+                result.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return result.ToString().Trim();
+        }        
+    }
+}
---
+++ Generics - Exercise/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string inputLine = Console.ReadLine();
+
+                box.AddItem(inputLine);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
